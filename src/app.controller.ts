63cfdad4dbc0094968a647f980038445cb3522e8
seungwoo <sw6016@hanmail.net>
2026-04-19 12:13:46 +0900

    One to One Relationship 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { ProfileModel } from './entity/profile.entity';
 import { UserModel } from './entity/user.entity';
 
 @Controller()
@@ -8,6 +9,8 @@ export class AppController {
   constructor(
     @InjectRepository(UserModel)
     private readonly userRepository: Repository<UserModel>,
+    @InjectRepository(ProfileModel)
+    private readonly profileRepository: Repository<ProfileModel>,
   ) {}
 
   @Post('users')
@@ -17,7 +20,11 @@ export class AppController {
 
   @Get('users')
   getHello() {
-    return this.userRepository.find({});
+    return this.userRepository.find({
+      relations: {
+        profile: true,
+      },
+    });
   }
 
   @Patch('users/:id')
@@ -30,7 +37,17 @@ export class AppController {
 
     return this.userRepository.save({
       ...user,
-      title: user?.title + '0',
     });
+  }
+
+  @Post('user/profile')
+  async createUserAndProfile() {
+    const user = await this.userRepository.save({ email: 'asdf@test.ai' });
+    const profile = await this.profileRepository.save({
+      profileImage: 'test.png',
+      user,
+    });
+
+    return user;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,12 @@ import {
   SingleBaseModel,
 } from './entity/inheritance.entity';
 import { StudentModel, TeacherModel } from './entity/person.entity';
+import { ProfileModel } from './entity/profile.entity';
 import { UserModel } from './entity/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserModel]),
+    TypeOrmModule.forFeature([UserModel, ProfileModel]),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: '127.0.0.1',
@@ -31,6 +32,7 @@ import { UserModel } from './entity/user.entity';
         SingleBaseModel,
         ComputerModel,
         AirplaneModel,
+        ProfileModel,
       ],
       synchronize: true,
     }),
